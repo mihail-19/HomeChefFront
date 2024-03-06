0aa6dc43f380bd1563570303a576b00758c58b09
mihail-19 <mihail.teslenko19@gmail.com>
2024-03-06 17:41:04 +0200

    responsive

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -3,10 +3,9 @@ import {Link} from 'react-router-dom'
 const Header = ()=>{
     return (
         <div className='header'>
-            <div className='header__container'>
                 <div className='header__logo'>Hoome Chef</div>
                 <div className='header__search'>
-                    <input type="text" value='Знайти за назвою страви'></input>
+                    <input type="text" value='Назва страви'></input>
                     <button className='header__search-button'>Знайти</button>
                 </div>
                 <nav className='header__nav'>
@@ -18,16 +17,13 @@ const Header = ()=>{
                         <li className='header__menu-item'>
                             <Link to='/dishes' className='header__menu-link'>Шефи на мапі</Link>
                         </li>
-                        <li className='header__menu-item'>
-                            <Link to='/dishes' className='header__menu-link'>Наші шефи</Link>
-                        </li>
+                        
                     </ul>
                 </nav>
                 <div className='header__auth'>
                     <Link to='/login' className='header__signin-button'>Увійти</Link>
                     <Link to='/register' className='header__register-button'>Реєстрація</Link>
                 </div>
-            </div>
         </div>
     )
 }
